e17aa67f6bcdc20b6e412d15cc5b1673d3735cf8
King J <1185657109@qq.com>
2017-09-02 10:52:50 +0800

    ModeRead v1.1
    将类说明放到头文件里。

--- EImC/ModeRead.cpp
+++ EImC/ModeRead.cpp
@@ -1,30 +1,4 @@
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <vector>
-#include <fstream>
-#include <string>
-#define BUFLEN 80       //缓冲区大小
-using namespace std;
-class ModeRead
-{
-private:
-    int readType;           //输入方式：文件或直接输入
-    int lineLen;          //缓冲区内的数据长度
-    int readPos;         //读取位置
-    char line[BUFLEN];      //缓冲区
-    int lineNum;         //行号
-    int colNum;           //列号
-    char lastch;         //上一个字符
-    FILE *file;         //文件指针
-    vector <char> in_content;//直接输入内容
-    char filePathName[1024];//文件路径
-public:
-    ModeRead();
-    int readMode();//运行模块入口
-    char scan();//读取字符
-    //void output();
-};
+#include "ModeRead.h"
 
 ModeRead::ModeRead()//初始化
 {

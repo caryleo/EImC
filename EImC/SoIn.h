d201fa89356a014e7b745a05ef7f0675177a6612
GRACE <GRACE@DESKTOP-C8KTR2U>
2017-09-04 21:57:10 +0800

    soint/out

--- EImC/SoIn.h
+++ EImC/SoIn.h
@@ -2,14 +2,13 @@
 #define SOIN_H_
 
 #include<iostream>
-#include"EImC.h"
 #include<vector>
 using namespace std;
 class SoIn
 {
 public:
-	static void input(Token *top, Token *bottom);
-	static void judgeIdt(Token *m);//用过来分析Idt的类型并输出
+	static void input(int top, int bottom);
+	static void judgeIdt(int m);//用过来分析Idt的类型并输出
 };
 
 

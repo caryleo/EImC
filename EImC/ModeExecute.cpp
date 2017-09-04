21a611f7c95f98e3899ae5da1423e5852c5e89b5
bitvincent <896112698@qq.com>
2017-09-04 11:04:10 +0800

    Revert "zhan"
    This reverts commit 3713c4887209ea7732de772529b5bcc313cf33ff.

--- EImC/ModeExecute.cpp
+++ EImC/ModeExecute.cpp
@@ -1,10 +1,5 @@
 #include "stdafx.h"
 #include "EImC.h"
 #include "ModeTokenAnalysis.h"
-#include "Stack.h"
-
-Stack runtime;
-Token ** rbp, rsp;
-
 
 
--- EImC/ModeExecute.h
+++ EImC/ModeExecute.h
@@ -1,11 +1,8 @@
 #ifndef MODEEXECUTE_H_
 #define MODEEXECUTE
-#include "EImC.h"
-#include "Stack.h"
-
 
 class ModeExecute {
-	
+ 
 };
 
 #endif // !MODEEXECUTE_H_

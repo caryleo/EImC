7c89e229c8cbeb37a22869f8c413ca19c555468f
GRACE <GRACE@DESKTOP-C8KTR2U>
2017-09-04 17:14:02 +0800

    so

--- EImC/EImC.cpp
+++ EImC/EImC.cpp
@@ -10,28 +10,8 @@ std::vector<Token*>buffer;
 
 int main()
 {
-	ModeRead mRead;
-	mRead.readMode();
-	cout << "Start token analyzing..." << endl;
-	ModeTokenAnalysis::read(mRead);
-	for (int i = 0; i < buffer.size(); i++) {
-		cout << buffer[i]->tag << endl;
-		if (buffer[i]->tag == NUM) {
-			Token * t = buffer[i];
-			SoInt * i = (SoInt*)t;
-			cout << "NUM: " << i->val << endl;
-		}
-		if (buffer[i]->tag == RNUM) {
-			Token * t = buffer[i];
-			SoReal * i = (SoReal*)t;
-			cout << "RNUM: " << i->val << endl;
-		}
-		if (buffer[i]->tag == STRING) {
-			Token * t = buffer[i];
-			SoString * i = (SoString*)t;
-			cout << "STRING: " << i->str << endl;
-		}
-	}
-	return 0;
+	std::cout << "Hello world." << std::endl;
+	std::cout << "Hello world!" << std::endl;
+    return 0;
 }
 

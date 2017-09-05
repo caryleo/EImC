f746b4f462d76f21d781de78706ad6ceb90b1961
King J <1185657109@qq.com>
2017-09-05 15:37:14 +0800

    重写expression

--- EImC/Expression.h
+++ EImC/Expression.h
@@ -1,52 +1,40 @@
 #include "ModeTokenAnalysis.h"
-#include "Stack.h"
 
 class ExprIR
 {
-private:
-    Stack num;
-    Stack opr;
-	Token *pos;
 public:
-    //入口
-    Idt exprEnter(Token *head,Token *tail);
-    //运算符计算
-    Idt calculate(Idt);
-    //Token->Idt
-    Idt changeToken(Token *);
-    //获取变量值
-    Tag getValType(Idt);
-    string getStrVal(Idt);
-    short getIntVal(Idt);
-    double getRealVal(Idt);
+    //查询类型
+    Tag getValType(Token *);
+    //查询IDT asstype
+    Tag getAsstype(Token *);
+    //判断是否为IDT
 
+    bool isIDT(Token *);
+    //Token*向下转型为Idt*
+    Idt* TransIDT(Token *);
+    //Token*向下转型为SoString*
+    SoString *TransStr(Token *);
+    //Token*向下转型为SoInt*
+    SoInt *TransInt(Token *);
+    //Token*向下转型为SoReal*
+    SoReal *TransReal(Token *);
+
+    //查询值
+    string getStrVal(Token *);
+    short getIntVal(Token *);
+    float getRealVal(Token *);
+
+    //判断操作数,若不是操作数就是操作符
+    bool isOperand(Token *);
     //运算符优先级
-    int oper_priority(Idt);
+    int oper_priority(Token *);
     //判断运算符优先级
-    int judge_priority(Idt,Idt);
-    //算数运算： +加 | -减 | *乘 | /除 | %余
-    Idt add_opr(Idt,Idt);
-    Idt sub_opr(Idt,Idt);
-    Idt mul_opr(Idt,Idt);
-    Idt div_opr(Idt,Idt);
-    Idt mod_opr(Idt,Idt);
-    //单目运算：+正 | -负
-    Idt pos_opr(Idt);
-    Idt neg_opr(Idt);
-    //括号运算：(左括号 | )右括号
-    //串操作：$连接 | #删一个字符
-    Idt connect_opr(Idt,Idt);
-    Idt deltail_opr(Idt);
-    Idt delspecial_opr(Idt,Idt);
-    //关系运算：>大于 | <小于 | >=大于等于 | <=小于等于 | ==恒等 | <>不等
-    //int Greater(Idt,Idt);
-    //int Greater_equal(Idt,Idt);
-    //int Less(Idt,Idt);
-    //int Less_equal(Idt,Idt);
-    //int Equal(Idt,Idt);
-    //int NEqual(Idt,Idt);
-    //逻辑运算：&&与 | ||或 | !非
-    //int And_opr(Idt,Idt);
-    //int Or_opr(Idt,Idt);
-    //int Not_opr(Idt);
+    bool judge_priority(Token * ,Token *);
+
+    //算术运算：ADD,SUB,MUL,DIV,MOD
+    Token * add_op(Token *,Token *);
+    Token * sub_op(Token *,Token *);
+    Token * mul_op(Token *,Token *);
+    Token * div_op(Token *,Token *);
+    Token * mod_op(Token *,Token *);
 };

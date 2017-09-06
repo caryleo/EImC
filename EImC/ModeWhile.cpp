a933b110428fc61d3552e977a03bbcdbb7d96be1
nuomizai <769572294@qq.com>
2017-09-06 17:38:40 +0800

    ModeWhile

---
+++ EImC/ModeWhile.cpp
@@ -0,0 +1,48 @@
+#include"ModeExecute.h"
+#include"ModeWhile.h"
+#include"Expression.h"
+#include"Stack.h"
+extern Token ** esp, **ebp;	//运行栈的栈顶和栈底
+extern Stack RunTime; //使用运行栈
+ModeWhile::ModeWhile(int _s,int _t,int _x,int _y)
+{
+    top=_s;
+    bottom=_t;
+    conTop=_x;
+    conBottom=_y;
+}
+bool ModeWhile::calcu()
+{
+    ExprIR *atom=new ExprIR;
+    Token *tmp=atom->calculate_expr(conTop ,conBottom );
+    if(tmp->tag==NUM)
+    {
+        if(((SoInt*)tmp)->val==0)
+            return 0;
+        else
+            return 1;
+    }
+    else if(tmp->tag==RNUM)
+    {
+        if(((SoReal*)tmp)->val==0)
+            return 0;
+        else
+            return 1;
+    }
+    else
+        return 0;
+}
+void ModeWhile::runWhile()
+{
+    if(calcu())
+    {
+        PRTR *tmp=new PRTR(ebp);
+        RunTime.push(tmp);
+        RunTime.sync(ebp);
+        RunTime.sync(esp);
+        ModeExecute::commence(top,bottom);
+    }
+    while(calcu())
+        ModeExecute::commence(top,bottom);
+    RunTime.desync(ebp, esp);
+}
